153f791ce4ac9748cc5dd0fc1e35458c8b7e3f54
thion96 <thion96@mail.ru>
2025-02-23 17:46:03 +0300

    complete task 2 in tests

--- tests/task_2/main.cpp
+++ tests/task_2/main.cpp
@@ -96,45 +96,70 @@ private:
 };
 
 
-TEST_CASE("Function `Empty` works correctly", "[List]") {
+TEST_CASE("Function `PushBack` works correctly", "[List]") {
     List list;
-    REQUIRE(list.Empty());
-
     list.PushBack(10);
-    REQUIRE_FALSE(list.Empty());
-
-    list.Clear();
-    REQUIRE(list.Empty());
-}
-
-
-TEST_CASE("Function `Size` works correctly", "[List]") {
-    List list;
-    REQUIRE(list.Size() == 0);
-
-    list.PushFront(7);
     REQUIRE(list.Size() == 1);
 
-    list.PushBack(8);
+    list.PushBack(20);
     REQUIRE(list.Size() == 2);
 
-    list.PopFront();
-    REQUIRE(list.Size() == 1);
-
-    list.Clear();
-    REQUIRE(list.Size() == 0);
+    REQUIRE(list.PopFront() == 10);
+    REQUIRE(list.PopFront() == 20);
 }
 
 
-TEST_CASE("Function `Clear` works correctly", "[List]") {
+TEST_CASE("Function `PushFront` works correctly", "[List]") {
     List list;
-    list.PushBack(1);
-    list.PushBack(2);
-    list.PushBack(3);
+    list.PushFront(30);
+    REQUIRE(list.Size() == 1);
 
-    REQUIRE(list.Size() == 3);
-    
-    list.Clear();
-    REQUIRE(list.Size() == 0);
+    list.PushFront(40);
+    REQUIRE(list.Size() == 2);
+
+    REQUIRE(list.PopBack() == 30);
+    REQUIRE(list.PopBack() == 40);
+}
+
+
+TEST_CASE("Function `PopBack` works correctly", "[List]") {
+    List list;
+    REQUIRE_THROWS_AS(list.PopBack(), std::runtime_error);
+
+    list.PushBack(50);
+    list.PushBack(60);
+    REQUIRE(list.PopBack() == 60);
+    REQUIRE(list.PopBack() == 50);
+    REQUIRE(list.Empty());
+}
+
+
+TEST_CASE("Function `PopFront` works correctly", "[List]") {
+    List list;
+    REQUIRE_THROWS_AS(list.PopFront(), std::runtime_error);
+
+    list.PushFront(70);
+    list.PushFront(80);
+    REQUIRE(list.PopFront() == 80);
+    REQUIRE(list.PopFront() == 70);
+    REQUIRE(list.Empty());
+}
+
+
+TEST_CASE("Complex usage", "[List]") {
+    List list;
+
+    list.PushFront(10);
+    list.PushBack(20);
+    list.PushFront(5);
+    list.PushBack(25);
+
+    REQUIRE(list.Size() == 4);
+
+    REQUIRE(list.PopFront() == 5);
+    REQUIRE(list.PopBack() == 25);
+    REQUIRE(list.PopFront() == 10);
+    REQUIRE(list.PopBack() == 20);
+
     REQUIRE(list.Empty());
 }
