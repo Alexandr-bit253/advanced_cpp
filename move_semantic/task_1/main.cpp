ef30e05c27a0583f85f6266ab14aaa2c634c0107
Yoda <yoda@example.com>
2025-03-19 08:31:17 +0300

    complete task 1 in move semantic

--- move_semantic/task_1/main.cpp
+++ move_semantic/task_1/main.cpp
@@ -2,8 +2,25 @@
 #include <vector>
 
 
-int main() {
+template <typename T>
+void move_vectors(std::vector<T>& src, std::vector<T>& dst) {
+	dst = std::move(src);
+}
 
+
+int main() {
+	std::vector <std::string> one = { "test_string1", "test_string2" };
+	std::vector <std::string> two;
+
+	std::cout << "Before moving:\n";
+	std::cout << "Vector one size: " << one.size() << "\n";
+	std::cout << "Vector two size: " << two.size() << "\n";
+
+	move_vectors(one, two);
+
+	std::cout << "After moving:\n";
+	std::cout << "Vector one size: " << one.size() << "\n";
+	std::cout << "Vector two size: " << two.size() << "\n";
 
 	return 0;
 }
