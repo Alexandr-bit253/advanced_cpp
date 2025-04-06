3f10e8bd5b8dce79a9bcc0fbff5f362d3670e219
thion96 <thion96@mail.ru>
2025-04-06 21:51:09 +0300

    add value_converter_tests.cpp

--- ini_parser/build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/OBI_1_KENOBI/source/repos/advanced_cpp/ini_parser/build/_deps/googletest-src
-# Build directory: C:/Users/OBI_1_KENOBI/source/repos/advanced_cpp/ini_parser/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
---
+++ ini_parser/tests/value_converter_tests.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include "..\include\ValueConverter.hpp"
+
+TEST(ValueConverterTest, IntConversion) {
+    EXPECT_EQ(ValueConverter<int>::convert("123"), 123);
+    EXPECT_THROW(ValueConverter<int>::convert("12.3"), ConversionError);
+}
+
+TEST(ValueConverterTest, BoolConversion) {
+    EXPECT_TRUE(ValueConverter<bool>::convert("true"));
+    EXPECT_FALSE(ValueConverter<bool>::convert("off"));
+    EXPECT_THROW(ValueConverter<bool>::convert("yes"));
+}
+
+TEST(ValueConverterTest, StringConversion) {
+    EXPECT_EQ(ValueConverter<std::string>::convert("Hello"), "Hello");
+}
